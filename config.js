58428f9e20c64bc4a6f409fdf5913eaad85720e6
chayentot <chayentot@gmail.com>
2026-07-14 14:28:21 +0800

    Update API_URL to the new Google Apps Script URL

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  API_URL: "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE"
+  API_URL: "https://script.google.com/macros/s/AKfycbxbUq6FEcrPKm24qqlzncGHBIMBINy0I8mPdsz6Qdkjp4LqNRGg392dwHnTYCVpKau30g/exec"
 };
